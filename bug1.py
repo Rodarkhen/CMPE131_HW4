4731c62900e372840ec5f574493ef1c9c0f066e7
RodrigoChen <rodrigo.chen@sjsu.edu>
2023-09-29 17:40:04 -0700

    Initial commit for the questions 1-5

---
+++ bug1.py
@@ -0,0 +1,39 @@
+class Base:
+# TODO: there's code missing in one or more lines below
+    def __init__(self,x,y,size):
+        self.x = x
+        self.y = y
+        self.size = size
+        
+class Circle(Base):
+    def __init__(self, x, size):
+        y = x
+        super().__init__(x, y, size)
+        '''self.x = x
+        self.y = y
+        self.size = size'''
+        
+    def shape(self):
+        print("This is a circle")
+        
+    def draw(self):
+        return f"""({self.x}, {self.y}) 
+{self.size}
+            , - ~ ~ ~ - ,
+        , '               ' ,
+      ,                      ,
+     ,                        ,
+    ,                          ,
+    ,                          ,
+    ,                          ,
+     ,                        ,
+      ,                      ,
+        ,                 , '
+          ' - , _ _ _ , '
+                 """
+def main():
+    s = Circle(2,1)
+    s.shape()
+    print(s.draw())
+    
+main()
---
+++ merge.py
@@ -0,0 +1,22 @@
+def merge_list(list1, list2):
+    new_list = []
+    new_list.extend(list1)
+    new_list.extend(list2)
+    for i in range(len(new_list)):
+        smallest_idx = i
+        for j in range(i + 1, len(new_list)):
+            if new_list[smallest_idx] > new_list[j]:
+                smallest_idx = j
+        '''
+        temp = new_list[i]
+        new_list[i] = new_list[smallest_idx]
+        new_list[smallest_idx] = temp'''
+        new_list[i], new_list[smallest_idx] = new_list[smallest_idx], new_list[i]
+    return new_list
+
+list1 = [1,5,9,11]
+list2 = [2,4,3,1]
+print("Input:  ", list1, ",", list2)
+sorted_list = merge_list(list1, list2)
+print(list1)
+print("Output: ", sorted_list)
