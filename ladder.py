4284b455d82d2218777713c53d9dfd3fa6b79c7d
RodrigoChen <rodrigo.chen@sjsu.edu>
2023-10-06 12:12:06 -0700

    ladder.py changed add raise ValueError

--- ladder.py
+++ ladder.py
@@ -1,20 +1,23 @@
 def my_steps(n):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    
-    # Create a list to store the number of ways to climb for each step.
-    steps = [0] * (n + 1)
-    
-    # Initialize values for the first two steps.
-    steps[1] = 1
-    steps[2] = 2
-    
-    # Calculate the number of ways to climb for each step from 3 to n.
-    for i in range(3, n + 1):
-        steps[i] = steps[i - 1] + steps[i - 2]
+    if n < 1 or n > 25:
+        raise ValueError("integer should be between 1 and 25")
+    else:
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        
+        # Create a list to store the number of ways to climb for each step.
+        steps = [0] * (n + 1)
+        
+        # Initialize values for the first two steps.
+        steps[1] = 1
+        steps[2] = 2
+        
+        # Calculate the number of ways to climb for each step from 3 to n.
+        for i in range(3, n + 1):
+            steps[i] = steps[i - 1] + steps[i - 2]
     
     return steps[n]
