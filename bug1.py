f69375a39ef8c28524b02fe06307bbd432dfc753
RodrigoChen <rodrigo.chen@sjsu.edu>
2023-10-10 18:32:23 -0700

    bug1.py modified

--- bug1.py
+++ bug1.py
@@ -9,16 +9,13 @@ class Circle(Base):
     Shape = "circle"
     def __init__(self, x, y, size):
         super().__init__(x, y, size)
-        '''self.x = x
-        self.y = y
-        self.size = size'''
         
     def shape(self):
         return f"This is a {self.Shape}"
         
     def draw(self):
-        return f"""({self.x}, {self.y})
-{self.size}
+        return f"""
+({self.x}, {self.y})\n{self.size}
          , - ~ ~ ~ - ,
      , '               ' ,
    ,                       ,
